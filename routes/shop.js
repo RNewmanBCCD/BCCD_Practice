9d507b3b6104f2b45af6ae9d4e81d78fb81ef8b7
Ryan Newman <rnewman@bccdistribution.com>
2024-05-14 11:21:09 -0400

    first set of changes

--- routes/shop.js
+++ routes/shop.js
@@ -17,6 +17,8 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/add-to-cart', shopController.addToCart);
+
 router.get('/checkout', shopController.getCheckout);
 
 router.get('/orders', shopController.getOrders);
